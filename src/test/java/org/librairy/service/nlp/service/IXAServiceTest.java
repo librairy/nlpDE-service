94dff1782bb9a217c95c896ebcb76cbc196ddb68
Carlos Badenes <cbadenes@gmail.com>
2018-05-30 11:37:28 +0200

    handle group of words

--- src/test/java/org/librairy/service/nlp/service/IXAServiceTest.java
+++ src/test/java/org/librairy/service/nlp/service/IXAServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.librairy.service.nlp.facade.model.Annotation;
 import org.librairy.service.nlp.facade.model.PoS;
+import org.librairy.service.nlp.facade.model.Token;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,13 +37,27 @@ public class IXAServiceTest {
     @Test
     public void annotation() throws IOException {
 
-        String text = "I have mice at my home";
+        String text = "Da gab der Mentor-Kapitän das verabredete Zeichen , und mit aller Kraft und Schnelligkeit rollten jetzt die Böller von der einen Schiffsseite nach der andern hinüber und schlugen , durch die dünne Wandung hindurch , auf das unten haltende , schon siegessichere Boot , das nun , von der Wucht der schweren eisernen Kanonen in Stücke gebrochen , mit Mann und Maus zugrunde ging";
 
         List<PoS> filter = Collections.emptyList();
 
         List<Annotation> annotations = service.annotate(text, filter);
 
         annotations.forEach(annotation -> LOG.info(annotation.toString()));
+
+//        Assert.assertEquals(2, annotations.size());
+    }
+
+    @Test
+    public void group() throws IOException {
+
+        String text = "Da gab der Mentor-Kapitän das verabredete Zeichen , und mit aller Kraft und Schnelligkeit rollten jetzt die Böller von der einen Schiffsseite nach der andern hinüber und schlugen , durch die dünne Wandung hindurch , auf das unten haltende , schon siegessichere Boot , das nun , von der Wucht der schweren eisernen Kanonen in Stücke gebrochen , mit Mann und Maus zugrunde ging";
+
+        List<PoS> filter = Collections.emptyList();
+
+        List<Token> groups = service.group(text, filter);
+
+        groups.forEach(annotation -> LOG.info(annotation.toString()));
 
 //        Assert.assertEquals(2, annotations.size());
     }
